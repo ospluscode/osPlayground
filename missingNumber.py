b7f2d2a454e0edf2dc78b91b660244be6b86c0f8
Orkhan Suleymanov <53385236+ospluscode@users.noreply.github.com>
2021-03-10 10:59:10 -0800

    Delete missingNumber.py

--- missingNumber.py
+++
@@ -1,8 +0,0 @@
-
-# Find the missing number in an integer array of 1 to 100
-# Here I utilize n*(n+1) / 2 formula to calculate sum of n numbers
-
-def findMissingNumber(list, n):
-    sumWithFormula = n * (n+1) / 2
-    sumList = sum(list)
-    return sumWithFormula - sumList
